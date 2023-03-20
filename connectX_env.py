7ad5215445e8194ce5c2428fb0cc1a661721dba9
omarASC5 <ocobas19@avenues.org>
2023-03-20 16:45:09 -0400

    moved Q Learning code inside of a folder

--- connectX_env.py
+++
@@ -1,5 +0,0 @@
-from kaggle_environments import evaluate, make, utils
-env = make("connectx", debug=True)
-
-env.run(['random', 'random'])
-env.render(mode="ipython", width=600, height=500, header=False)
