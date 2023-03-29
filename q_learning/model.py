15f4b138c902918aa905ab4dfc0d2e1bde530983
Omar Cobas <omarlcobas@gmail.com>
2023-03-29 10:56:55 -0400

    switched from tensorflow to pytorch

--- q_learning/model.py
+++ q_learning/model.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-import tensorflow.lite as tflite
+import torch
+import torch.nn as nn
+import torch.optim as optim
 
 #https://gist.github.com/leeschmalz/1b733278792ce751f0a9c2d2de3323b0
 class Memory:
@@ -19,6 +20,10 @@ class Memory:
         self.rewards.append(float(new_reward))
 
 #https://gist.github.com/leeschmalz/fd5535477f276c5e9b965c6c1ea13cbd
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
 class Model:
     def __init__(self, name = '') -> None:
         self.model = self.create_model()
@@ -26,51 +31,50 @@ class Model:
         self.epsilon = 1
         self.reward = 0
         self.name = name
+        self.optimizer = optim.Adam(self.model.parameters())
 
     def create_model(self):
-        model = tf.keras.Sequential()
-        
-        model.add(tf.keras.layers.Flatten())
-        model.add(tf.keras.layers.Dense(50, activation='relu'))
-        model.add(tf.keras.layers.Dense(50, activation='relu'))
-        model.add(tf.keras.layers.Dense(50, activation='relu'))
-        model.add(tf.keras.layers.Dense(50, activation='relu'))
-        model.add(tf.keras.layers.Dense(50, activation='relu'))
-        model.add(tf.keras.layers.Dense(50, activation='relu'))
-        model.add(tf.keras.layers.Dense(50, activation='relu'))
-        
-        model.add(tf.keras.layers.Dense(7))
-
+        model = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(50, 50),
+            nn.ReLU(),
+            nn.Linear(50, 50),
+            nn.ReLU(),
+            nn.Linear(50, 50),
+            nn.ReLU(),
+            nn.Linear(50, 50),
+            nn.ReLU(),
+            nn.Linear(50, 50),
+            nn.ReLU(),
+            nn.Linear(50, 50),
+            nn.ReLU(),
+            nn.Linear(7, 7)
+        )
         return model
-   
-    def compute_loss(self, logits, actions, rewards): 
-        neg_logprob = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=actions)
-        loss = tf.reduce_mean(neg_logprob * rewards)
+
+    def compute_loss(self, logits, actions, rewards):
+        log_probs = torch.nn.functional.log_softmax(logits, dim=1)
+        action_log_probs = log_probs.gather(1, actions.unsqueeze(1)).squeeze(1)
+        loss = -1 * (action_log_probs * rewards).mean()
         return loss
-  
-    def train_step(self, optimizer, observations, actions, rewards):
-        with tf.GradientTape() as tape:
-        # Forward propagate through the agent network
-            
-            logits = self.model(observations)
-            loss = self.compute_loss(logits, actions, rewards)
-            grads = tape.gradient(loss, self.model.trainable_variables)
 
-            optimizer.apply_gradients(zip(grads, self.model.trainable_variables))
+    def train_step(self, observations, actions, rewards):
+        self.optimizer.zero_grad()
+        logits = self.model(observations)
+        loss = self.compute_loss(logits, actions, rewards)
+        loss.backward()
+        self.optimizer.step()
 
-    # Convert the TensorFlow model to a TensorFlow Lite model
     def convert_model(self, model_file_path):
         # Save the model to a file
-        tf.saved_model.save(self.model, model_file_path)
+        torch.save(self.model, model_file_path)
 
-        # Convert the model to a TensorFlow Lite model
-        converter = tflite.TFLiteConverter.from_saved_model(model_file_path)
-        tflite_model = converter.convert()
-
-        # Save the TensorFlow Lite model to a file
-        tflite_model_file_path = f'{model_file_path}/model.tflite'
-        with open(tflite_model_file_path, 'wb') as f:
-            f.write(tflite_model)
+        # Convert the model to a TorchScript model
+        traced_model = torch.jit.trace(self.model, torch.randn(1, 1, 28, 28))
+        
+        # Save the TorchScript model to a file
+        traced_model_file_path = f'{model_file_path}/model.pt'
+        traced_model.save(traced_model_file_path)
 
     def decay_epsilon(self):
         self.epsilon = self.epsilon * 0.99985
@@ -79,29 +83,3 @@ class Model:
         self.win_count = 0
         self.reward = 0
 
-def load_player_model(player_model_id, player_model_name):
-    # TODO: Lookup the player model in the blockchain/Oracle
-    # TODO: Return it, if found. Otherwise, return a new model
-    # retrieved_model = db.lookup(player_model_id)
-    retrieved_model = None
-
-    if retrieved_model is None:
-        print(f'Could not find player model with id {player_model_id} and name {player_model_name}')
-        print('Creating a new one from scratch with random weights...')
-        retrieved_model = Model(player_model_name)
-
-    return retrieved_model
-
-def save_player_model(player_model_id, player_model):
-    # TODO: Save the player model to the blockchain/Oracle
-    # Return the response
-    # response = db.save(player_model_id, player_model)
-    # return response
-    pass
-
-def save_actions(player_model_id, actions):
-    # TODO: Save the actions to the blockchain/Oracle
-    # Return the response
-    # response = db.save(player_model_id, actions)
-    # return response
-    pass
