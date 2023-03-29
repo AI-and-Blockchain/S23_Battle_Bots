fc309ce777f0ee76a9168467fa79e4540c8a103c
ml619 <70662865+ml619@users.noreply.github.com>
2023-03-29 17:41:49 -0400

    Add files via upload

--- betting/bet.py
+++ betting/bet.py
@@ -7,65 +7,113 @@ def apporve():
     
     
     cleanup = Seq(
-        App.localDel(Txn.sender(), Bytes("bet amount")),
-        App.localDel(Txn.sender(), Bytes("game outcome")),
-        App.localDel(Txn.sender(), Bytes("winner")),
-        App.localDel(Txn.sender(), Bytes("bot")),
+        App.localDel(Txn.applications_args[1], Bytes("bet amount")),
+        App.localPut(Txn.applications_args[1], Bytes("bet ready"),Bytes("N/A")),
+        App.localPut(Txn.applications_args[1], Bytes("bot"),Bytes("N/A")),
+        App.localDel(Txn.applications_args[1], Bytes("bet bot")),
         
-        App.localDel(App.localGet(Txn.sender(), Bytes("opponent")), Bytes("bet amount")),
-        App.localDel(App.localGet(Txn.sender(), Bytes("opponent")), Bytes("game outcome")),
-        App.localDel(App.localGet(Txn.sender(), Bytes("opponent")), Bytes("winner")),
-        App.localDel(App.localGet(Txn.sender(), Bytes("opponent")), Bytes("bot")),
-        App.localDel(App.localGet(Txn.sender(), Bytes("opponent")), Bytes("opponent")),
+        App.localDel(App.localGet(Txn.applications_args[1], Bytes("opponent")), Bytes("bet amount")),
+        App.localPut(App.localGet(Txn.applications_args[1], Bytes("opponent")), Bytes("bet ready"), Bytes("N/A")),
+        App.localPut(App.localGet(Txn.applications_args[1], Bytes("opponent")), Bytes("bot"),Bytes("N/A")),
+        App.localDel(App.localGet(Txn.applications_args[1], Bytes("opponent")), Bytes("bet bot")),
+        App.localDel(App.localGet(Txn.applications_args[1], Bytes("opponent")), Bytes("opponent")),
         
-        App.localDel(Txn.sender(), Bytes("opponent")),
+        App.localDel(Txn.applications_args[1], Bytes("opponent")),
     )
     
-    payout_sender = Seq(
+    payout = Seq(opp.store(App.localGet(Txn.applications_args[1], Bytes("opponent"))),
+        Cond([App.localGet(opp.load(),Bytes("bet bot")) == Int(0),
         InnerTxnBuilder.Begin(),
         InnerTxnBuilder.SetFields({
             TxnField.type_enum: TxnType.Payment,
             TxnField.amount: App.localGet(Txn.sender(), Bytes("bet")) + App.localGet(App.localGet(Txn.sender(), Bytes("opponent")), Bytes("bet")),
-            TxnField.receiver: Txn.sender()
+            TxnField.receiver: Txn.applications_args[1]
         }),
-        InnerTxnBuilder.Submit()
-    )
-    payout_opponent = Seq(
-        InnerTxnBuilder.Begin(),
-        InnerTxnBuilder.SetFields({
-            TxnField.type_enum: TxnType.Payment,
-            TxnField.amount: App.localGet(Txn.sender(), Bytes("bet")) + App.localGet(App.localGet(Txn.sender(), Bytes("opponent")), Bytes("bet")),
-            TxnField.receiver: App.localGet(Txn.sender(), Bytes("opponent"))
-        }),
-        InnerTxnBuilder.Submit()
-    )
-    
-    start_game = Seq(App.localPut(Txn.sender(), Bytes("game outcome"),Int(0)),
-        While(App.localGet(Txn.sender(), Bytes("game outcome")) == Int(0)).Do(
-            Seq(
-                
-            )
+        InnerTxnBuilder.Submit()],
+        [App.localGet(App.localGet(Txn.applications_args[1], Bytes("opponent")),Bytes("bet bot")) == Int(1),
+         App.localPut(Txn.applications_args[1],App.localGet(opp.load(),(Bytes("bot"))),Int(1)),
+         App.localDel(opp.load(),App.localGet(opp.load(),(Bytes("bot"))))]
         ),
-        Cond([App.localGet(Txn.sender(), Bytes("winner")) == 1, payout_sender],[True, payout_opponent]),
         cleanup
-        )
-    
-    
-    
+    )
+
     #record bet ammount, bot and opponent of sender
     #if the opponent also has the sender as thier opponent start the game else just approve the transaction
-    bet = Seq(App.localPut(Txn.sender(), Bytes("bet amount"),Txn.amt),
-              App.localPut(Txn.sender(), Bytes("bot"),Txn.applications_args[1]),
-               App.localPut(Txn.sender(), Bytes("opponent"),Txn.applications_args[2]),
-               Cond([App.localGet(Bytes("opponent"), Bytes("opponent")) == Txn.sender(), start_game], [True, Approve()])
-              )
+    bet = Cond([Txn.applications_args[2] != Bytes("bot"),Cond([App.localGet(Txn.sender(),Bytes("balance")) >= Btoi(Txn.applications_args[2]),
+                Seq(
+                    App.localPut(Txn.sender(), Bytes("bet bot"),Int(0)),
+                    App.localPut(Txn.sender(), Bytes("bet amount"),Btoi(Txn.applications_args[2])),
+                    App.localPut(Txn.sender(), Bytes("opponent"),Txn.applications_args[3]),
+                    Cond([App.localGet(Txn.sender(),Txn.applications_args[4]) == Int(1),App.localPut(Txn.sender(), Bytes("bot"),Txn.applications_args[4])]),
+                    Cond([App.localGet(Txn.sender(),Bytes("bet ready")) == Bytes("N/A"),#fix for malicous bets
+                          Seq(App.localPut(Txn.sender(),Bytes("bet ready"),Bytes("false")),App.localPut(Txn.applications_args[3],Bytes("bet ready"),Bytes("false")))],              
+                          [App.localGet(Txn.sender(),Bytes("bet ready")) == Bytes("false"),  
+                            Seq(App.localPut(Txn.sender(),Bytes("bet ready"),Bytes("true")),App.localPut(Txn.applications_args[3],Bytes("bet ready"),Bytes("true")))])                       
+                )
+                
+                ])],[Txn.applications_args[2] == Bytes("bot"), 
+                    Seq(
+                        App.localPut(Txn.sender(), Bytes("bet bot"),Int(1)),
+                        App.localPut(Txn.sender(), Bytes("opponent"),Txn.applications_args[3]),
+                        Cond([App.localGet(Txn.sender(),Txn.applications_args[4]) == Int(1),App.localPut(Txn.sender(), Bytes("bot"),Txn.applications_args[4])]), 
+                        Cond([App.localGet(Txn.sender(),Bytes("bet ready")) == Bytes("N/A"),#fix for malicous bets
+                          Seq(App.localPut(Txn.sender(),Bytes("bet ready"),Bytes("false")),App.localPut(Txn.applications_args[3],Bytes("bet ready"),Bytes("false")))],              
+                          [App.localGet(Txn.sender(),Bytes("bet ready")) == Bytes("false"),  
+                            Seq(App.localPut(Txn.sender(),Bytes("bet ready"),Bytes("true")),App.localPut(Txn.applications_args[3],Bytes("bet ready"),Bytes("true")))])
+                    ) 
+                    ]
+            )
+    
+    process_payment = App.localPut(Txn.sender(),Bytes("balance"),App.localGet(Txn.sender(),Bytes("balance"))+Txn.amt)
+    
+    withdrawal = Cond([App.localGet(Txn.sender(),Bytes("balance")) >= Btoi(Txn.applications_args[1]),
+                        Seq(
+                                App.localPut(Txn.sender(),Bytes("balance"),App.localGet(Txn.sender(),Bytes("balance"))-Txn.applications_args[1]),
+                                InnerTxnBuilder.Begin(),
+                                InnerTxnBuilder.SetFields({
+                                    TxnField.type_enum: TxnType.Payment,
+                                    TxnField.receiver: Txn.sender(),
+                                    TxnField.amount: Txn.applications_args[1]
+                                }),
+                                InnerTxnBuilder.Submit()
+                            )
+                    ])
+                        
+    create_bot = Seq(
+                        App.localPut(Txn.sender(),App.globalGet((Bytes("num bots"))),Int(1)),
+                        App.globalPut((Bytes("num bots"),App.globalGet((Bytes("num bots")))+Int(1)))
+                    )
     
     
     
+    optin = Seq(
+                App.localPut(Txn.sender(),Bytes("balance"),Int(0)),
+                App.localPut(Txn.sender(),Bytes("bet ready"),Bytes("N/A")),
+                App.localPut(Txn.sender(),Bytes("bot"),Bytes("N/A")),
+                )
     
-    no_op = Cond([Txn.applications_args[0] == Bytes("bet") and Txn.TxType = Bytes("pay"), bet])
+    no_op = Cond([Txn.type == Bytes("pay"),process_payment],
+                 [Txn.applications_args[0] == Bytes("bet")and 
+                  App.localGet(Txn.sender(),Bytes("bet ready")) == Bytes("N/A"), bet],
+                 [Txn.applications_args[0] == Bytes("withdrawal"), withdrawal],
+                 [Txn.applications_args[0] == Bytes("resolve"), payout],
+                 [Txn.applications_args[0] == Bytes("create bot"),create_bot],
+                 [Txn.applications_args[0] == Bytes("transfer bot") and 
+                  App.localGet(Txn.sender(),Bytes("bot")) != Txn.applications_args[1],transfer_bot],
+                 [Txn.applications_args[0] == Bytes("delete bot") and 
+                  App.localGet(Txn.sender(),Bytes("bot")) != Txn.applications_args[1],delete_bot],
+                 )
     
-    return Cond([Txn.on_completion() == "NoOp", no_op])
+    setup = App.globalPut(Bytes("num bots"),Int(0))
+    
+    delete_bot = App.localDel(Txn.sender(),Txn.applications_args[1])
+    
+    transfer_bot = Seq(
+                        App.localPut(Txn.applications_args[2],Txn.applications_args[1],Int(1)),
+                        App.localDel(Txn.sender(),Txn.applications_args[1]),
+                       )
+    
+    return Cond([Txn.application_id() == Int(0), setup],[Txn.on_completion() == "NoOp", no_op],[Txn.on_completion() == "Optin", optin])
 
 def clear():
-    pass
+    Return(1)
