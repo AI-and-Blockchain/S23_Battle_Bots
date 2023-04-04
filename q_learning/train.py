c72b3c9edbd022c83e5f8cc18cb121a3e7fd9209
Omar Cobas <omarlcobas@gmail.com>
2023-04-04 15:30:02 -0400

    progress

--- q_learning/train.py
+++ q_learning/train.py
@@ -1,12 +1,12 @@
 import torch
 import numpy as np
 from kaggle_environments import make
-from model import BattleBot, Game, load_bot, load_game, Memory
+from model import BattleBot, Game, load_bot, Memory
 from connect4 import Connect4
 
 LEARNING_RATE = 0.001
 
-def play_battle_bots(board, env, memory, player_1_model, player_2_model):
+def play_battle_bots(board, env, memory, player_1_bot, player_2_bot):
     print('Playing Battle Bots...')
 
     # Reset the environment and get the initial state of the board
@@ -15,9 +15,10 @@ def play_battle_bots(board, env, memory, player_1_model, player_2_model):
     observation = trainer.reset()['board']
 
     # Reset the models for both players and decay the epsilon value
-    players = (player_1_model, player_2_model)
+    players = (player_1_bot, player_2_bot)
     for player in players:
-        player.reset()
+        print(player)
+        player.reward = 0
         player.decay_epsilon()
 
     current_player = None
@@ -27,9 +28,9 @@ def play_battle_bots(board, env, memory, player_1_model, player_2_model):
     while True:
         # Player 1 and Player 2 alternate taking turns
         if i % 2 == 0:
-            current_player = player_1_model
+            current_player = player_1_bot
         else:
-            current_player = player_2_model
+            current_player = player_2_bot
 
         # Find the next action for the player to take
         action, _ = board.get_action(current_player, observation, current_player.epsilon)
@@ -40,7 +41,7 @@ def play_battle_bots(board, env, memory, player_1_model, player_2_model):
         # and observe the new board state
         try:
             next_observation, _, overflow, _ = trainer.step(action)
-        except FailedPrecondition:
+        except Exception:
             observation = trainer.reset()['board']
             memory.clear()
 
@@ -61,12 +62,12 @@ def play_battle_bots(board, env, memory, player_1_model, player_2_model):
         # Update the model of both battle bots after the game is over
         if winner_found:
             print("Winner Found, Updating models...")
-            player_1_model.train_step(
+            player_1_bot.train_step(
                     observations=torch.cat(memory.observations),
                     actions=torch.tensor(memory.actions),
                     rewards = torch.tensor(memory.rewards))
             
-            player_2_model.train_step(
+            player_2_bot.train_step(
                     observations=torch.cat(memory.observations),
                     actions=torch.tensor(memory.actions),
                     rewards = torch.tensor(memory.rewards))
@@ -75,7 +76,7 @@ def play_battle_bots(board, env, memory, player_1_model, player_2_model):
 
         i += 1
 
-    return player_1_model, player_2_model, actions, current_player
+    return player_1_model, player_2_bot, actions, current_player.name
 
 
 if __name__ == '__main__':
@@ -87,26 +88,30 @@ if __name__ == '__main__':
     player_1_bot_name = 'BattleBotA'
     player_1_bot = load_bot(player_1_bot_id)
     if player_1_bot is None:
-        player_1_bot = BattleBot(player_1_bot_id, player_1_bot_name, '', None)
+        print('Creating new bot for player 1...')
+        player_1_bot = BattleBot(player_1_bot_name, player_1_bot_id, f'./models/{player_1_bot_name}.pt', None)
+        player_1_bot.save_bot()
 
     player_2_bot_id = 'B'
     player_2_bot_name = 'BattleBotB'
     player_2_bot = load_bot(player_2_bot_id)
     if player_2_bot is None:
-        player_2_bot = BattleBot(player_2_bot_id, player_2_bot_name, '', None)
+        print('Creating new bot for player 2...')
+        player_2_bot = BattleBot(player_2_bot_name, player_2_bot_id, f'./models/{player_2_bot_name}.pt', None)
+        player_2_bot.save_bot()
 
     # Create the Connect 4 board
     board = Connect4()
 
     # Have the two battle bots from the players compete against each other
-    new_player_1_model, new_player_2_model, actions, winner = play_battle_bots(board, env, memory, player_1_bot, player_2_bot)
+    player_1_bot, player_2_bot, actions, winner_name = play_battle_bots(board, env, memory, player_1_bot, player_2_bot)
 
-    # Update the player models associated with each player
-    player_1_bot.update_model(new_player_1_model)
-    player_2_bot.update_model(new_player_2_model)
+    # # Update the player models associated with each player
+    # player_1_bot.save_bot()
+    # player_2_bot.save_bot()
 
-    # Save the game to the database
-    game_id = '1'
-    game = Game(game_id, player_1_bot, player_2_bot, winner.name, actions)
-    game.save_game()
+    # # Save the game to the database
+    # game_id = '1'
+    # game = Game(game_id, player_1_bot, player_2_bot, winner_name, actions)
+    # game.save_game()
 
