601bbafd9c6e32071d4bcd2dd8d4d1e7de94e6f6
Omar Cobas <omarlcobas@gmail.com>
2023-04-05 08:40:23 -0400

    added comments, and switched to using IDs for actions

--- q_learning/model.py
+++ q_learning/model.py
@@ -7,23 +7,26 @@ import torch.optim as optim
 
 #https://gist.github.com/leeschmalz/1b733278792ce751f0a9c2d2de3323b0
 class Memory:
-    def __init__(self): 
+    '''Memory buffer for storing the observations, actions, and rewards'''
+    def __init__(self):
         self.clear()
 
-    # Resets/restarts the memory buffer
-    def clear(self): 
+    def clear(self):
+        '''Clears the memory buffer'''
         self.observations = []
         self.actions = []
         self.rewards = []
         self.info = []
         
-    def add_to_memory(self, new_observation, new_action, new_reward): 
+    def add_to_memory(self, new_observation, new_action, new_reward):
+        '''Adds the new observation, action, and reward to the memory buffer'''
         self.observations.append(new_observation)
         self.actions.append(new_action)
         self.rewards.append(float(new_reward))
 
 
 class BattleBot:
+    '''A battle bot that can play the game of Connect 4'''
     def __init__(self, name, bot_id, model_path, model):
         self.name = name
         self.bot_id = bot_id
@@ -35,21 +38,26 @@ class BattleBot:
         self.win_count = 0
         self.total_games = 0
 
+        # Create the model for the battle bot if one was not passed in
         if model is None:
             self.model = self.create_model()
         else:
             self.model = model
+
+        # Create the optimizer for the model, using the Adam optimizer
         self.optimizer = optim.Adam(self.model.parameters())
 
+        # Initialize the bots data file if it does not exist
         if not os.path.exists(self.bots_file_path):
             with open(self.bots_file_path, "w") as f:
                 json.dump([], f)
 
-        # Save the model to a file
+        # Initialize the models directory if it does not exist
         if not os.path.exists('./models'):
             os.makedirs('./models')
 
     def save_bot(self):
+        '''Saves the battle bot to the bots data file'''
         with open(self.bots_file_path, "r") as f:
             bots_data = json.load(f)
 
@@ -63,10 +71,13 @@ class BattleBot:
             "model_path": self.model_path,
         }
 
+        # Check if the bot already exists in the bots data file
         prev_bot = load_bot(self.bot_id)
         if prev_bot is None:
+            # If the bot does not exist, add it to the bots data file
             bots_data.append(bot_data)
         else:
+            # If the bot does exist, update the bot's data in the bots data file
             for bot_data in bots_data:
                 if bot_data['bot_id'] == self.bot_id:
                     bot_data['win_count'] = self.win_count
@@ -75,6 +86,7 @@ class BattleBot:
                     bot_data['reward'] = self.reward
                     break
 
+        # Save the bots data file with the updated bots list
         with open(self.bots_file_path, "w") as f:
             json.dump(bots_data, f)
 
@@ -85,6 +97,7 @@ class BattleBot:
 
 
     def create_model(self):
+        # Create the model for the battle bot using a simple deep fully connected neural network
         model = nn.Sequential(
             nn.Linear(42, 50),
             nn.ReLU(),
@@ -104,12 +117,14 @@ class BattleBot:
         return model
     
     def compute_loss(self, logits, actions, rewards):
+        # Compute the loss using the logits, actions, and rewards
         log_probs = torch.nn.functional.log_softmax(logits, dim=1)
         action_log_probs = log_probs.gather(1, actions.unsqueeze(1)).squeeze(1)
         loss = -1 * (action_log_probs * rewards).mean()
         return loss
 
     def train_step(self, observations, actions, rewards):
+        # Take a train step for the model using the observations, actions, and rewards
         self.optimizer.zero_grad()
         observations = observations.view(-1, 42).to(torch.float32)
         logits = self.model(observations)
@@ -118,17 +133,13 @@ class BattleBot:
         self.optimizer.step()
 
     def decay_epsilon(self):
+        # Decay the epsilon value for the battle bot
+        # Overtime the battle bot will become more greedy and less random
         self.epsilon = self.epsilon * 0.99985
 
-    def reset(self):
-        self.win_count = 0
-        self.reward = 0
-
-    def update_stats(self):
-        self.total_games += 1
-        self.win_count += 1
 
 class Game:
+    '''Stores a game of Connect 4, including the game ID, player IDs, winner ID, and actions.'''
     def __init__(self, game_id, player_1_id, player_2_id, winner_id, actions):
         self.game_id = game_id
         self.player_1_id = player_1_id
@@ -137,11 +148,14 @@ class Game:
         self.actions = actions
         self.games_file_path = './games.json'
 
+        # Initialize the games data file if it does not exist
         if not os.path.exists(self.games_file_path):
             with open(self.games_file_path, "w") as f:
                 json.dump([], f)
 
     def save_game(self):
+        '''Saves the game to the games data file'''
+        # Save the game to the games data file
         with open(self.games_file_path, "r") as f:
             games_data = json.load(f)
 
@@ -153,30 +167,86 @@ class Game:
             "actions": self.actions,
         }
 
+        # Check if the game already exists in the games data file
         prev_game = load_game(self.game_id)
         if prev_game is None: 
+            # If the game does not exist, add it to the games data file
             games_data.append(game_data)
         else:
+            # If the game does exist, update the game's data in the games data file
             for game_data in games_data:
                 if game_data['game_id'] == self.game_id:
                     game_data['winner_id'] = self.winner_id
                     game_data['actions'] = self.actions
                     break
 
+        # Save the games data file with the updated games list
         with open(self.games_file_path, "w") as f:
             json.dump(games_data, f)
 
         print("Inserted game document with ID:", self.game_id)
 
 
+class Trainer:
+    '''This class is responsible for setting up the training environment for the battle bots.
+    It replaces the kaggle connect4 environment through our own, more flexible environment.'''
+    def __init__(self):
+        self.num_rows = 6
+        self.num_cols = 7
+        self.board = []
+
+        # Initialize the board
+        for _ in range(self.num_rows):
+            self.board.append([0] * self.num_cols)
+
+    def reset(self):
+        '''Reset the board to all zeros.'''
+        for i in range(self.num_rows):
+            for j in range(self.num_cols):
+                self.board[i][j] = 0
+
+    def column_overflow(self, action):
+        '''Check if the column has overflowed for the given action.'''
+        assert(action >= 0 and action < self.num_cols)
+        # If any of the top row is non-zero, then the board has overflowed
+        for i in range(self.num_rows):
+            if self.board[i][action] == 0:
+                return False
+
+        return True
+
+    def step(self, action, player):
+        '''Take a step on the board for the given player. Takes the action in that given column.'''
+        assert(player == 1 or player == 2)
+        assert(action >= 0 and action < self.num_cols)
+
+        # If the column is full, then the action is invalid
+        # That means there is no room in that given column for the current action
+        if self.column_overflow(action):
+            return False
+
+        # Place the player's token in the top of the given column, if there is room
+        # Falls as if there is gravity
+        for i in range(self.num_rows - 1, -1, -1):
+            if self.board[i][action] == 0:
+                self.board[i][action] = player
+                return True
+
+        return False
+
+
 def load_bot(bot_id):
+    '''Loads a bot from the bots data file using the bot ID.'''
+    # Initialize the bots data file if it does not exist
     if not os.path.exists('./bots.json'):
         with open('./bots.json', "w") as f:
             json.dump([], f)
 
+    # Load the bots data file
     with open('./bots.json', "r") as f:
         bots_data = json.load(f)
 
+    # Find the bot with the given bot ID
     for bot_data in bots_data:
         if bot_data['bot_id'] == bot_id:
             print("Found bot with bot_id", bot_id)
@@ -195,13 +265,16 @@ def load_bot(bot_id):
     return None
 
 def load_game(game_id):
+    '''Loads a game from the games data file using the game ID.'''
     if not os.path.exists('./games.json'):
         with open('./games.json', "w") as f:
             json.dump([], f)
 
+    # Load the games data file
     with open('./games.json', "r") as f:
         games_data = json.load(f)
 
+    # Find the game with the given game ID
     for game_data in games_data:
         if game_data['game_id'] == game_id:
             print("Found game with game_id", game_id)
