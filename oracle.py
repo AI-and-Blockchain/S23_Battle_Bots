b3dffea1db61aeea666b5bd4b6cb2f699c7b4854
Ryan <kerrr@rpi.edu>
2023-04-12 20:13:54 -0400

    WORKING

--- oracle.py
+++ oracle.py
@@ -1,14 +1,12 @@
-from algosdk import account, encoding, mnemonic
-from algosdk.transaction import Transaction
-from q_learning.model import BattleBot, Game, load_bot, Memory, Trainer
+from algosdk import account, encoding, mnemonic, transaction
+from q_learning.model import BattleBot, Game, load_bot, Memory
 from q_learning.connect4 import Connect4
 from algosdk.v2client import algod
-from algosdk.wallet import Wallet
 from pyteal import *
 from q_learning.train import play_battle_bots
 from algosdk.v2client import indexer
 import algosdk
-import time, os, base64, uuid
+import time, base64, uuid
 
 # Connect to Algorand node
 algod_address = "https://testnet-algorand.api.purestake.io/ps2"
@@ -20,12 +18,15 @@ my_client = algod.AlgodClient(algod_token, algod_address, headers)
 apid = 0 # PUT APP ID HERE
 app_address = "" # PUT APP ADDRESS HERE
 
+privateKey = "" #private key for sending winner
+publicAdd = "" #public key for sending winner
+pa = encoding.decode_address(publicAdd)
 
+gameDone = True
 # [run game]
-# pull model from MongloDB database in model.py and call play_battle_bots in train.py
 
 def runGame(player1ID, player2ID, botID1, botID2):
-
+    gameDone = False
     memory = Memory()
     # Load the bots for both players
     player_1_bot_id = botID1
@@ -58,11 +59,14 @@ def runGame(player1ID, player2ID, botID1, botID2):
     if winner_id == player_1_bot.bot_id:
         winner_name = player_1_bot.name
         player_1_bot.win_count += 1
+        callContract(player1ID, player1ID, player2ID, False)
     elif winner_id == player_2_bot.bot_id:
         winner_name = player_2_bot.name
         player_2_bot.win_count += 1
+        callContract(player2ID, player1ID, player2ID, False)
     elif winner_id == 'NotValidWinnerID':
         winner_name = 'No Winner Found'
+        callContract(winner_id, player1ID, player2ID, True)
 
     game_id = str(uuid.uuid4())
     game = Game(game_id, player_1_bot.bot_id, player_2_bot.bot_id, winner_name, actions)
@@ -80,10 +84,7 @@ def runGame(player1ID, player2ID, botID1, botID2):
 
     print(f'Winner: {winner_name}!!!')
 
-    updateWebsite(player_1_bot_id, player_2_bot_id, actions, winner_id)
-    callContract(winner_id)
-
-    pass
+    gameDone = True
 
 # [update and send info]
 # with the return from play_battle_bots update models in database, update 
@@ -91,104 +92,103 @@ def runGame(player1ID, player2ID, botID1, botID2):
 # as first arguement), winner id must be base32 encoded.
 # Connect to Algorand node
 
-def updateWebsite(player1ID, player2ID, actions, winner):
-    # send moves and winner to website
-    print("Not Implemented")
-
-def callContract(winner):
-    """
-    # send winner to smart contract
-
-    # Define the parameters for the transaction
-    params = my_client.suggested_params()
-    params.fee = 1000
-    params.flat_fee = True
-
-    # Define the arguments for the transaction
-    arguments = ["resolve".encode(), winner.encode()]
-
-
-    # Build the transaction
-    txn = Transaction(
-        sender=algod_address,
-        sp=params,
-        app_id=apid,
-        on_complete=OnComplete.NoOpOC.real,
-        note=bytes("Winner of the bet".encode()),
-        app_args=arguments,
+def wait_for_confirmation(client, txid):
+    last_round = client.status().get("last-round")
+    txinfo = client.pending_transaction_info(txid)
+    while not (txinfo.get("confirmed-round") and txinfo.get("confirmed-round") > 0):
+        print("Waiting for confirmation...")
+        last_round += 1
+        client.status_after_block(last_round)
+        txinfo = client.pending_transaction_info(txid)
+    print(
+        "Transaction {} confirmed in round {}.".format(
+            txid, txinfo.get("confirmed-round")
+        )
     )
-    # Sign the transaction
-    signed_txn = txn.sign(algod_token)
+    return txinfo
 
-    # Send the transaction
-    tx_id = algod_client.send_transaction(signed_txn)
-    print(f"Transaction ID: {tx_id}")
+def call_app(client, private_key, pub,index, app_args, player1ID, player2ID):
+    # declare sender
+    sender = pub
+    print("Call from account:", sender)
 
-    """
-    print("Not Implemented")
+    # get node suggested parameters
+    params = client.suggested_params()
+    # comment out the next two (2) lines to use suggested fees
+    params.flat_fee = True
+    params.fee = 1000
 
+    # create unsigned transaction
+    txn = transaction.ApplicationNoOpTxn(sender, params, index, app_args,accounts=[player1ID,player2ID])
+
+    # sign transaction
+    signed_txn = txn.sign(private_key)
+    tx_id = signed_txn.transaction.get_txid()
+
+    # send transaction
+    client.send_transactions([signed_txn])
+
+    # await confirmation
+    wait_for_confirmation(client, tx_id)
+
+def callContract(winner, player1ID, player2ID, tie):
+    if (tie):
+        args = [b'tie', encoding.decode_address(player1ID)]
+    else:
+        args = [b'resolve', encoding.decode_address(winner)]
+
+    call_app(my_client, privateKey, publicAdd ,apid, args, player1ID, player2ID)
 
 # [recieve game information]
 # check algorand blockchain every few seconds and if any users local state's game 
 # ready variable is set to 'true' then pull model id and opponent model id
 
 # Define oracle logic
-"""
-def process_event(event):
-    if event['type'] == 'application' and event['application-index'] == program:
-    
-        # Retrieve the sender and application ID from the event data
-        sender = event['txn']['txn']['sender']
-        app_id = event['txn']['txn']['application-id']
-
-        # Retrieve the bot and opponent values from local state using the sender and app ID
-        bot = algod_client.account_info(sender)['apps-local-state'][str(app_id)][bytes("bot").hex()]
-        opponent = algod_client.account_info(sender)['apps-local-state'][str(app_id)][bytes("opponent").hex()]
-
-        # Print the bot and opponent values to the console
-        print(f"Bot: {bot}")
-        print(f"Opponent: {opponent}")
-"""
-
 
 # Wait for events
 def wait_for_events():
     # Initialize the indexer client
     testnet_address = "https://testnet-algorand.api.purestake.io/idx2"
     idx_client = indexer.IndexerClient(algod_token, testnet_address, headers)
-    last_txn_id = 28973160
+    last_txn_id = 0
     # Loop indefinitely
     while True:
-        print("Searching for Transactions...")
-        # Get the transactions of the smart contract with a higher ID than the last processed ID
-        txns = idx_client.search_transactions_by_address(
-            #application_id=apid,
-            address = app_address,
-            min_round=last_txn_id + 1
-        )
+        if gameDone:
+            print("Searching for Transactions...")
+            # Get the transactions of the smart contract with a higher ID than the last processed ID
+            txns = idx_client.search_transactions(
+                #address = app_address,
+                application_id = apid,
+                min_round=last_txn_id + 1
+            )
 
-        # Iterate over the new transactions and extract the user ID
-        
-        opponents = {}
-        botid = {}
-        for txn in txns["transactions"]:
-            #print("transaction\n", txn, "\n")
-            local_state = idx_client.lookup_account_application_local_state(txn['sender'], include_all = True)
-            for pair in local_state['apps-local-states'][0]['key-value']:
-                if base64.b64decode(pair['key']).decode('utf-8') == 'bot':  
-                    botid[txn['sender']] = str(int.from_bytes(base64.b64decode(pair['value']['bytes']), 'big'))
-            if txn['sender'] in opponents:
-                print(botid)
-                runGame(txn['sender'], opponents[txn['sender']], botid[txn['sender']], botid[opponents[txn['sender']]])
-            else:
-                for pair in local_state['apps-local-states'][0]['key-value']:
-                    if base64.b64decode(pair['key']).decode('utf-8') == 'opponent':
-                        opponents[algosdk.encoding.encode_address(base64.b64decode(pair['value']['bytes']))] = txn['sender']
+            # Iterate over the new transactions and extract the user ID
 
-            # Update the last processed ID
-            last_txn_id = max(last_txn_id, txn["confirmed-round"])
+            opponents = {}
+            botid = {}
+            for txn in txns["transactions"]:
+                if (txn['tx-type'] == 'appl') and ('local-state-delta' in txn.keys()):
+                    local_state = txn['local-state-delta'][0]['delta']
+                    for pair in local_state:
+                        if base64.b64decode(pair['key']).decode('utf-8') == 'bot': 
+                            try: 
+                                botid[txn['sender']] = str(int.from_bytes(base64.b64decode(pair['value']['bytes']), 'big'))
+                            except:
+                                pass
+                    if txn['sender'] in opponents:
+                        print(botid)
+                        runGame(txn['sender'], opponents[txn['sender']], botid[txn['sender']], botid[opponents[txn['sender']]])
+                    else:
+                        for pair in local_state:
+                            if base64.b64decode(pair['key']).decode('utf-8') == 'opponent':
+                                try:
+                                    opponents[algosdk.encoding.encode_address(base64.b64decode(pair['value']['bytes']))] = txn['sender']
+                                except:
+                                    pass
+                    # Update the last processed ID
+                last_txn_id = max(last_txn_id, txn["confirmed-round"])
         # Wait for a few seconds before checking for new transactions again
-        time.sleep(5)
+        time.sleep(10)
 
 # Main function
 def main():
